489f15f3388812d55f4b7c13953a11c944ed9771
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-03-21 20:59:23 +0200

    subject controller fixed to use new functions in base controller

--- backend/src/controllers/subject/subjects.controller.ts
+++ backend/src/controllers/subject/subjects.controller.ts
@@ -1,16 +1,15 @@
 import {
   Body,
   Controller,
-  Logger,
   Post,
   Put,
   Res,
 } from '@nestjs/common';
-import { Subject } from 'src/repositories/subject/schema';
-import { SubjectService } from 'src/services/subject/subject.service';
+import { Subject } from '../../repositories/subject/schema'; 
+import { SubjectService } from '../../services/subject/subject.service';
 import { BaseController } from '../base-controller';
 import { FastifyReply } from 'fastify';
-import { MongoError } from 'mongodb';
+import { IActionResult } from '../response-helpers/action-result.interface';
 
 @Controller('subjects')
 export class SubjectController extends BaseController {
@@ -20,19 +19,19 @@ export class SubjectController extends BaseController {
   }
 
   @Post()
-  async create(@Body() subject: Subject, @Res() response: FastifyReply) {
+  async create(@Body() subject: Subject, @Res() response: FastifyReply): Promise<IActionResult> {
     try {
       const result = await this.
       subjectsService.create(subject);
       
       if(!result.created) {
-        this.badRequest(response, result);
+       return this.badRequest(result);
       }
       else{
-        this.created(response, result)
+        return this.created(result)
       }
     } catch(error) {
-      this.handleSubjectError(error, response, subject)
+        return this.internalServerError(error)
     }
   }
 
@@ -41,25 +40,13 @@ export class SubjectController extends BaseController {
     try{
       const result = await this.subjectsService.update(subject)
       if(result.modifiedCount < 1){
-          this.notFound(response)
+        return this.notFound(response)
       }
       else{
-          this.ok(response)
+        return this.ok(response)
       }
     }catch(error){
-      this.handleSubjectError(error, response, subject)
+      return this.internalServerError(error)
     }
   }
-
-  //#region Helpers
-  private handleSubjectError(error: any, response: FastifyReply, subject: Subject): void{
-    Logger.error(error);
-
-    if(error instanceof MongoError && error.code === 11000){
-      this.badRequest(response, { message: `Subject With name '${subject.name}' already exists`})
-    }else{
-      this.internalServerError(response);
-    }
-  }
-  //#endregion
 }
