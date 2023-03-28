bd56aba1e9628e0190506c1e494ff26066f99a27
hamzarasheed52 <rasheedhamza005@gmail.com>
2023-03-28 17:59:07 +0500

    FE for notifcations

---
+++ frontend/src/components/NotificationWindow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="notification-window">
+    <div class="x-between px-20">
+      <h4 class="font-size-16 mb-0">Notifications</h4>
+      <i class="fa fa-times my-21 clickable"></i>
+    </div>
+    <NotificationList/>
+    <div>
+      <p class="medium clickable view-all-btn py-30">View all notifications</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import NotificationList from './NotificationList.vue'
+export default {
+  components: {
+    NotificationList
+  }
+}
+</script>
