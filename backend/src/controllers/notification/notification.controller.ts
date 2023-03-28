7daf354b898adc4934998a8f0db6eb4cf71528ef
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-03-28 23:10:40 +0200

    notification controller added

---
+++ backend/src/controllers/notification/notification.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { NotificationManager } from '../../managers/notification/notification.manager';
+import { BaseController } from '../base-controller';
+import { IActionResult } from '../response-helpers/action-result.interface';
+import { NotificationDto } from './notification.dto';
+
+@Controller()
+export class NotificationController extends BaseController {
+
+    constructor(private readonly notificationManager: NotificationManager) {
+        super();
+    }
+
+    @Post('trigger/:event')
+    async trigger(@Param('event') event: string, @Body() notificationDto: NotificationDto ) : Promise<IActionResult> {
+        await this.notificationManager.notify(event, notificationDto.actionUrl, notificationDto.payload ,notificationDto.to)
+        return this.ok();
+    }
+}
