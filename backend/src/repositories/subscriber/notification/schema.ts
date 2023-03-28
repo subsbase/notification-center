22e859c2f98c93e7c970158b049cf5b07d418ae9
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-03-28 23:08:04 +0200

    subscriber notification updated

--- backend/src/repositories/subscriber/notification/schema.ts
+++ backend/src/repositories/subscriber/notification/schema.ts
@@ -1,20 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Schema as MongoSchema } from 'mongoose';
+import { SchemaOptions } from '../../schema.options';
+import { BaseModel } from '../../base-model';
 import { Topic } from '../../topic/schema';
 
-@Schema()
-export class Notification {
+@Schema(SchemaOptions)
+export class Notification extends BaseModel {
   @Prop({ required: true, type: MongoSchema.Types.ObjectId, ref: 'Topic' })
   topic: Topic;
 
   @Prop({ required: true })
   content: string;
 
-  @Prop({ required: true, default: new Date().getUTCDate(), type: Date })
-  createdAt: Date;
+  @Prop()
+  actionUrl: string;
 
   @Prop({ required: true, default: false, type: Boolean })
   read: boolean;
+
+
+  static create(topic: Topic, content: string, actionUrl: string ): Notification {
+    let notification = new Notification();
+
+    notification.topic = topic;
+    notification.actionUrl = actionUrl
+    notification.content = content
+
+    return notification;
+  }
 }
 
 export const NotificationSchema = SchemaFactory.createForClass(Notification);
