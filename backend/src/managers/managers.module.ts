2da57fb4b8cf884467a7de35de73a4fad584581d
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-03-29 00:54:48 +0200

    subject manager to managers module

--- backend/src/managers/managers.module.ts
+++ backend/src/managers/managers.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ServicesModule } from '../services/services.module';
 import { NotificationManager } from './notification/notification.manager';
+import { SubjectManager } from './subject/subject.manager';
 import { SubscribersManager } from './subscriber/subscriber.manager';
 import { TopicsManager } from './topic/topics.manager';
 
 @Module({
     imports: [ServicesModule],
-    providers: [SubscribersManager, TopicsManager, NotificationManager],
-    exports: [SubscribersManager,TopicsManager, NotificationManager]
+    providers: [SubscribersManager, SubjectManager,TopicsManager, NotificationManager],
+    exports: [SubscribersManager, SubjectManager ,TopicsManager, NotificationManager]
 })
 export class ManagersModule {}
