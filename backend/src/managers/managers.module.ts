15cd4b3f0c173c0073aa169a96587a942e696b14
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-03-29 01:02:29 +0200

    remove subscribers manager

--- backend/src/managers/managers.module.ts
+++ backend/src/managers/managers.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { ServicesModule } from '../services/services.module';
 import { NotificationManager } from './notification/notification.manager';
 import { SubjectManager } from './subject/subject.manager';
-import { SubscribersManager } from './subscriber/subscriber.manager';
 import { TopicsManager } from './topic/topics.manager';
 
 @Module({
     imports: [ServicesModule],
-    providers: [SubscribersManager, SubjectManager,TopicsManager, NotificationManager],
-    exports: [SubscribersManager, SubjectManager ,TopicsManager, NotificationManager]
+    providers: [SubjectManager,TopicsManager, NotificationManager],
+    exports: [SubjectManager ,TopicsManager, NotificationManager]
 })
 export class ManagersModule {}
