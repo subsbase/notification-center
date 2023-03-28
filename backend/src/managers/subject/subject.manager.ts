c2e20a16c0bd03eeb76f41b6cd60060bec2b0b72
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-03-28 22:54:20 +0200

    subject manager added

---
+++ backend/src/managers/subject/subject.manager.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { Subject } from "../../repositories/subject/schema";
+import { CreatedModel, UpdatedModel } from "../../repositories/helper-types";
+import { SubjectService } from "../../services/subject/subject.service";
+
+@Injectable()
+export class SubjectManager {
+    constructor(private readonly subjectService: SubjectService) {}
+    
+    async create(subject: Subject) : Promise<CreatedModel> {
+        return await this.subjectService.create(subject);
+    }
+
+    async update(subject: Subject): Promise<UpdatedModel> {
+        return await this.subjectService.update(subject)
+    }
+}
