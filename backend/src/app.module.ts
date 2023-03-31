f052b880e33d62acbb61186e9fae63d0d2f2f29e
Ibrahim Ahmed Badr <56772211+Ibrahimogod@users.noreply.github.com>
2023-04-01 00:29:02 +0200

    Feature/notifc 5 refactor configuration injection (#4)
    * repository module refactored
    
    * services module refactored
    
    * managers module refactored
    
    * controller module refactored
    
    * refactor app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ControllersModule],
+  imports: [ConfigModule.forRoot(), ControllersModule.withConfig(process.env.MONGODB_CONNECTION as string)],
 })
 export class AppModule {}
--- backend/src/services/services.module.ts
+++ backend/src/services/services.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { RepositoriesModule } from '../repositories/repositories.module';
 import { NotificationTemplateService } from './notification-template/notification-template.service';
 import { NotificationProcessor } from './notification/notification.processor';
@@ -7,9 +7,14 @@ import { SubjectService } from './subject/subject.service';
 import { SubscriberService } from './subscriber/subscriber.service';
 import { TopicService } from './topic/topic.service';
 
-@Module({
-  imports: [RepositoriesModule],
-  providers: [SubjectService, TopicService, SubscriberService, NotificationProcessor, NotificationService , NotificationTemplateService, TopicService],
-  exports: [SubjectService, TopicService, NotificationService, NotificationTemplateService],
-})
-export class ServicesModule {}
+@Module({})
+export class ServicesModule {
+  static withDbonnection(uri: string): DynamicModule{
+    return {
+      module: ServicesModule,
+      imports: [RepositoriesModule.withUri(uri)],
+      providers: [SubjectService, TopicService, SubscriberService, NotificationProcessor, NotificationService , NotificationTemplateService, TopicService],
+      exports: [SubjectService, TopicService, NotificationService, NotificationTemplateService],
+    }
+  }
+}
