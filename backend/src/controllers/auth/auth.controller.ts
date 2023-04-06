2d6b160a406080ff7d700a3e23b3ba79a4531b60
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-06 20:29:56 +0200

    fix auth controller

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
 import { AuthManager } from '../../managers/auth/auth.manager';
 import { BaseController } from '../base-controller';
+import { AuthDto } from './auth.dto';
+import { IActionResult } from '../response-helpers/action-result.interface';
+import { FastifyRequest } from '../../types/global-types';
 
 @Controller('auth')
 export class AuthController extends BaseController {
-  constructor(private readonly authManager: AuthManager) {
-    super();
+  constructor(
+    @Inject(REQUEST) protected readonly request: FastifyRequest,
+    private readonly authManager: AuthManager) {
+    super(request);
   }
-
-  @Post('login')
-  signIn() {
-    const jwtToken = this.authManager.signIn()
+  
+  @Post()
+  async authenticate(@Body() authDto: AuthDto) : Promise<IActionResult> {
+    const jwtToken = await this.authManager.signIn(authDto.apiSecret)
     return this.ok(jwtToken);
   }
 }
