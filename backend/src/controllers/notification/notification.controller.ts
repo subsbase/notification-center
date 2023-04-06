341b03ac886a130e741e0c2875caefa527ec994a
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-06 20:29:17 +0200

    authorize trigger endpoint

--- backend/src/controllers/notification/notification.controller.ts
+++ backend/src/controllers/notification/notification.controller.ts
@@ -5,6 +5,7 @@ import { IActionResult } from '../response-helpers/action-result.interface';
 import { NotificationDto } from './notification.dto';
 import { REQUEST } from '@nestjs/core';
 import { FastifyRequest } from '../../types/global-types';
+import { Authorize } from '../decorators/authorize.decorator';
 
 @Controller()
 export class NotificationController extends BaseController {
@@ -15,6 +16,7 @@ export class NotificationController extends BaseController {
         super(request);
     }
 
+    @Authorize()
     @Post('trigger/:event')
     async trigger(@Param('event') event: string, @Body() notificationDto: NotificationDto ) : Promise<IActionResult> {
         await this.notificationManager.notify(event, notificationDto.actionUrl, notificationDto.payload ,notificationDto.to)
