2d37389da80777794454e41a4979297ef7ba678a
Ibrahim Ahmed Badr <56772211+Ibrahimogod@users.noreply.github.com>
2023-04-10 19:23:33 +0200

    Feature/notifc 12 add GitHub workflows for ci cd (#10)
    * edit env sample
    
    * add host config for application
    
    * add Dockerfile
    
    * add docker compose for local test
    
    * add backend dockerization flow
    
    * add frontend dockerization flow

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.setGlobalPrefix('notifc')
   app.useGlobalFilters(new MongoErrorFilter(),new ValidationErrorFilter());
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 
   Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
