55d7fddb4ca7772c60db89caaf31a5bc4d27f20b
Ibrahim Ahmed Badr <56772211+Ibrahimogod@users.noreply.github.com>
2023-05-01 04:33:22 +0200

    Feature/notifc 20 use socket for changes notification (#19)
    * add event emitter  deps
    
    * add internal events module and events
    
    * import internal events module
    
    * handle subscriber joind
    
    * use event emitter with the socket
    
    * emit notifications events

---
+++ backend/src/internal-events/notification.ts
@@ -0,0 +1,72 @@
+export class NotificationEvent {
+
+    constructor(
+        subscriberId: string,
+        notificationsIds: Array<string>) {
+        this.subscriberId = subscriberId;
+        this.notificationsIds = notificationsIds;
+    }
+
+    subscriberId: string;
+    notificationsIds: Array<string>;
+}
+
+export class NotificationArchived extends NotificationEvent {
+
+    constructor(
+        subscriberId: string,
+        notificationsIds: Array<string>) {
+        super(subscriberId, notificationsIds)
+    }
+}
+
+export class NotificationUnarchived extends NotificationEvent {
+
+    constructor(
+        subscriberId: string,
+        notificationsIds: Array<string>) {
+            super(subscriberId, notificationsIds)
+    }
+}
+
+
+export class NotificationRead extends NotificationEvent {
+
+    constructor(
+        subscriberId: string,
+        notificationsIds: Array<string>) {
+            super(subscriberId, notificationsIds)
+
+    }
+}
+
+export class NotificationUnread extends NotificationEvent {
+
+    constructor(
+        subscriberId: string,
+        notificationsIds: Array<string>) {
+            super(subscriberId, notificationsIds)
+
+    }
+}
+
+export class NotificationsEvent {
+
+    constructor(subscriberId: string) { 
+        this.subscriberId = subscriberId       
+    }
+
+    subscriberId: string;
+}
+
+export class NotificationsRead extends NotificationsEvent {
+    constructor(subscriberId: string) { 
+       super(subscriberId)       
+    }
+}
+
+export class NotificationsUnread extends NotificationsEvent {
+    constructor(subscriberId: string) { 
+       super(subscriberId)       
+    }
+}
