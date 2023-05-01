be4a076ff64d92e3f5df29100f724a3a707e8453
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-01 05:32:16 +0300

    use event emitter with the socket

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -5,22 +5,36 @@ import {
   WebSocketServer,
   WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
 import { Notification } from "../repositories/subscriber/notification/schema";
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { NotificationArchived, NotificationEvent, NotificationUnarchived, NotificationsEvent } from '../internal-events/notification';
+import { Injectable } from '@nestjs/common';
 
 @WebSocketGateway()
 export class EventsGateway {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
   @WebSocketServer()
   private server: Server;
 
   @SubscribeMessage('joinGroup')
   joinGroup(@MessageBody() subscriberId: string) {
     this.server.socketsJoin(subscriberId)
+    this.eventEmitter.emit('subscriber.joined', subscriberId)
   }
     
   notifySubscribers(notification: Notification, subscriberIds: Array<string>) {
     this.server.to(subscriberIds).emit("notification", notification)
+  }
+
+  @OnEvent('notification.*')
+  handleNotificationEvent(payload: any){
+    this.server.to(payload.subscriberId).emit(payload.constructor.name, payload.notificationsIds)
+  }
+
+  @OnEvent('notifications.*')
+  handleNotificationsEvent(payload: any){
+    this.server.to(payload.subscriberId).emit(payload.constructor.name.toString())
   }
 }
