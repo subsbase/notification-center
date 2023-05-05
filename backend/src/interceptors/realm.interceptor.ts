ad2816c40cb694933a05aeb7416b85f8f0581432
Ibrahim Ahmed Badr <56772211+Ibrahimogod@users.noreply.github.com>
2023-05-06 01:16:14 +0300

    Feature/notifc 25 add ignore realm (#26)
    * add IgnoreRealm decorator
    
    * handle ignore realm in interceptor
    
    * Use ignore realm on healthz endpoint

--- backend/src/interceptors/realm.interceptor.ts
+++ backend/src/interceptors/realm.interceptor.ts
@@ -1,11 +1,27 @@
-import { BadRequestException, CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { BadRequestException, CallHandler, ExecutionContext, Inject, NestInterceptor } from "@nestjs/common";
 import { FastifyRequest } from "fastify";
 import { Observable } from "rxjs";
 import { GlobalContext } from "../types/global-context";
+import { Reflector } from "@nestjs/core";
 
 export class RealmInterceptor implements NestInterceptor {
 
+    constructor(
+        @Inject(Reflector)
+        private readonly reflector: Reflector) { }
+    
+
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+
+        const ignoreRealm = this.reflector.get<boolean>('ignoreRealm', context.getHandler());
+
+        if(!ignoreRealm) {
+            this.SetGlobalContexRealm(context);
+        }   
+        return next.handle();
+    }
+
+    private SetGlobalContexRealm(context: ExecutionContext) {
         const ctx = context.switchToHttp();
         const request = ctx.getRequest<FastifyRequest>();
         const realm = request.headers['x-realm'] as string | undefined;
@@ -14,8 +30,7 @@ export class RealmInterceptor implements NestInterceptor {
             throw new BadRequestException('x-realm header is required')
         }
 
-        GlobalContext.Realm = realm!;    
-        return next.handle();
+        GlobalContext.Realm = realm!; 
     }
 
 }
