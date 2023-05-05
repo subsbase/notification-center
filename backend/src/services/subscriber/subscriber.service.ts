ebaac64db5387715c5e713cc810bbd25461274e4
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-06 00:55:51 +0300

    remove 'subscriber.joined' event handler function

--- backend/src/services/subscriber/subscriber.service.ts
+++ backend/src/services/subscriber/subscriber.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { CreatedModel } from '../../repositories/helper-types';
 import { SubscribersRepository } from '../../repositories/subscriber/repository';
 import { Subscriber } from '../../repositories/subscriber/schema';
-import { OnEvent } from '@nestjs/event-emitter';
 
 @Injectable()
 export class SubscriberService {
@@ -15,17 +14,5 @@ export class SubscriberService {
 
     async create(subscriber: Subscriber) : Promise<CreatedModel> {
         return await this.subscriberRepository.create(subscriber);
-    }
-
-    @OnEvent('subscriber.joined')
-    async handleSubscriberJoind(payload: any) {
-        //todo: make sure realm inclueded in payload
-        await this.createOrUpdate({
-            subscriberId: payload.subscriberId,
-            realm: payload.realm,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            _id: ''
-        })
     }
 }
