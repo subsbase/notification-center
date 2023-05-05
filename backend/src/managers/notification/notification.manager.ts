27f43c98a739d2268177e9a657e73ce6e43026e1
Ibrahim Ahmed Badr <56772211+Ibrahimogod@users.noreply.github.com>
2023-05-06 00:38:54 +0300

    Feature/notifc 21 create topics and subjects onthe fly (#22)
    * add findOrCreate
    
    * add get or create topic and subject
    
    * handle notification subject and topic creation on the fly

--- backend/src/managers/notification/notification.manager.ts
+++ backend/src/managers/notification/notification.manager.ts
@@ -1,17 +1,19 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { EventsGateway } from '../../events/events.gateway';
 import { Notification } from '../../repositories/subscriber/notification/schema';
 import { NotificationService } from '../../services/notification/notification.service';
 import { TopicService } from '../../services/topic/topic.service';
 import { Payload } from "../../types/global-types";
 import { UpdatedModel } from '../../repositories/helper-types';
+import { SubjectService } from '../../services/subject/subject.service';
 
 @Injectable()
 export class NotificationManager {
     constructor(
             private readonly eventsGateway: EventsGateway,
             private readonly notificationService: NotificationService,
-            private readonly topicsService: TopicService) { }
+            private readonly topicsService: TopicService,
+            private readonly subjectService: SubjectService) { }
             
 
     async getAllNotifications(subscriberId: string, pageNum: number, pageSize: number) : Promise<Array<Notification>> {
@@ -52,17 +54,17 @@ export class NotificationManager {
     }
 
     async notify(
+        subjectName: string,
         event: string,
         actionUrl: string,
         payload: Payload,
         subscribersIds: Array<string>
         ) {
 
-        const topic = await  this.topicsService.getByEvent(event);
-
-        if(!topic){
-            throw new NotFoundException(null, `No Topic found with event ${event}`)
-        }
+        const subject = await this.subjectService.getOrCreate(subjectName)
+        
+        //gets or creates topic by the event
+        const topic = await this.topicsService.getByEvent(event, subject)
 
         const notificationTemplate = topic.notificationTemplate;
 
