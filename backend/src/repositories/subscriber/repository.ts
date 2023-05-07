194fab8d253642b29138e20b817b6bb0fa47ded8
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-07 22:26:34 +0300

    import base repository

--- backend/src/repositories/subscriber/repository.ts
+++ backend/src/repositories/subscriber/repository.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { BaseRepository } from '../base-repository';
 import { Subscriber, SubscriberDocument } from './schema';
+import { BaseRepository } from '../base-repository';
 
 @Injectable()
 export class SubscribersRepository extends BaseRepository<SubscriberDocument, Subscriber> {
     
     constructor(
     @InjectModel(Subscriber.name)
-    protected readonly model: Model<SubscriberDocument>) {
-        super(model);
+    protected readonly model: Model<SubscriberDocument>,
+    protected readonly realm: string) {
+        super(model, realm);
     }
 }
