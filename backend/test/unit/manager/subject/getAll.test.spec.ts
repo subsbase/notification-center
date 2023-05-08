43e08f6a4fc319154fdf06bea31f24c28bf66bc3
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-09 02:10:20 +0300

    introduce subject manager tests

---
+++ backend/test/unit/manager/subject/getAll.test.spec.ts
@@ -0,0 +1,79 @@
+import { SubjectManager } from '../../../../src/managers/subject/subject.manager';
+import { SubjectService } from '../../../../src/services/subject/subject.service';
+import { Subject } from '../../../../src/repositories/subject/schema';
+import { createMock } from '@golevelup/ts-jest';
+
+describe('SubjectManager getAll', () => {
+  let subjectManager: SubjectManager;
+  let subjectService: SubjectService;
+
+  beforeEach(async () => {
+    subjectService = createMock<SubjectService>();
+    subjectManager = new SubjectManager(subjectService);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should call subjectService.getAll with a pageNum and pageSize of 0', async () => {
+    const pageNum = 0;
+    const pageSize = 0;
+
+    jest.spyOn(subjectService, 'getAll').mockResolvedValue([]);
+
+    const result = await subjectManager.getAll(pageNum, pageSize);
+
+    expect(result).toEqual([]);
+    expect(subjectService.getAll).toHaveBeenCalledWith(pageNum, pageSize);
+  });
+
+  it('should call subjectService.getAll with a pageNum value greater than the total number of pages available', async () => {
+    const pageNum = 100;
+    const pageSize = 10;
+
+    jest.spyOn(subjectService, 'getAll').mockResolvedValue([]);
+
+    const result = await subjectManager.getAll(pageNum, pageSize);
+
+    expect(result).toEqual([]);
+    expect(subjectService.getAll).toHaveBeenCalledWith(pageNum, pageSize);
+  });
+
+  it('should call subjectService.getAll with a pageSize value greater than the total number of items available', async () => {
+    const pageNum = 1;
+    const pageSize = 100;
+
+    const expected = [new Subject(), new Subject()];
+    jest.spyOn(subjectService, 'getAll').mockResolvedValue(expected);
+
+    const result = await subjectManager.getAll(pageNum, pageSize);
+
+    expect(result).toEqual(expected);
+    expect(subjectService.getAll).toHaveBeenCalledWith(pageNum, pageSize);
+  });
+
+  it('should call subjectService.getAll with a pageNum value less than 1', async () => {
+    const pageNum = -1;
+    const pageSize = 10;
+
+    jest.spyOn(subjectService, 'getAll').mockResolvedValue([]);
+
+    const result = await subjectManager.getAll(pageNum, pageSize);
+
+    expect(result).toEqual([]);
+    expect(subjectService.getAll).toHaveBeenCalledWith(pageNum, pageSize);
+  });
+
+  it('should call subjectService.getAll with a pageSize value less than 1', async () => {
+    const pageNum = 1;
+    const pageSize = -10;
+
+    jest.spyOn(subjectService, 'getAll').mockResolvedValue([]);
+
+    const result = await subjectManager.getAll(pageNum, pageSize);
+
+    expect(result).toEqual([]);
+    expect(subjectService.getAll).toHaveBeenCalledWith(pageNum, pageSize);
+  });
+});
