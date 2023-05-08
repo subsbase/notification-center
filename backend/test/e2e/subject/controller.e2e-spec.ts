0adaa6da563d12c70f714ff367bb58c82e49cc97
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-09 02:15:18 +0300

    run npm format

--- backend/test/e2e/subject/controller.e2e-spec.ts
+++ backend/test/e2e/subject/controller.e2e-spec.ts
@@ -1,7 +1,7 @@
 import * as request from 'supertest';
 import mongoose from 'mongoose';
-import { HttpStatus, INestApplication } from "@nestjs/common";
-import { Test } from '@nestjs/testing'
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ResponseInterceptor } from '../../../src/interceptors/response.interceptor';
 import { MongoErrorFilter } from '../../../src/filters/mongo-error.filter';
@@ -12,47 +12,49 @@ import { Subject } from '../../../src/repositories/subject/schema';
 import { Reflector } from '@nestjs/core/services/reflector.service';
 
 describe('subjects', () => {
-    let app: INestApplication;
-  
-    beforeEach(async () => {
-      const moduleRef = await Test.createTestingModule({
-        imports: [ConfigModule.forRoot({
-            envFilePath: '.env.test'
-        }), ControllersModule.withConfig(process.env.MONGODB_CONNECTION as string)],
-      })
-        .compile();
-  
-        app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
-        const reflector = new Reflector()
-        await app.useGlobalInterceptors(new ResponseInterceptor(reflector))
-        await app.useGlobalFilters(new MongoErrorFilter(), new ValidationErrorFilter());
-        await app.init();
-        await app.getHttpAdapter().getInstance().ready();
-    });
-  
-    afterAll(async () => {
-        await app.close();
-        const mongodb = await mongoose.connect(process.env.MONGODB_CONNECTION as string)
-        await mongodb.connection.db.dropDatabase();
-    });
-    
-    it('/POST subjects', () => {
-       return request(app.getHttpServer())
-            .post('/subjects')
-            .send({ name: 'Invoice' })
-            .then(response => {
-                expect(response.statusCode).toBe(HttpStatus.CREATED)
-                expect(response.body.created).toBeTruthy()
-            })
-    });
+  let app: INestApplication;
 
-    it('/GET subjects', () => {
-        return request(app.getHttpServer())
-            .get('/subjects')
-            .then(response => {
-                expect(response.statusCode).toBe(HttpStatus.OK)
-                expect(response.body instanceof Array<Subject>).toBeTruthy()
-                expect(response.body[0].name).toBe('Invoice')
-            })
-    })
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+        }),
+        ControllersModule.withConfig(process.env.MONGODB_CONNECTION as string),
+      ],
+    }).compile();
+
+    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+    const reflector = new Reflector();
+    await app.useGlobalInterceptors(new ResponseInterceptor(reflector));
+    await app.useGlobalFilters(new MongoErrorFilter(), new ValidationErrorFilter());
+    await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    const mongodb = await mongoose.connect(process.env.MONGODB_CONNECTION as string);
+    await mongodb.connection.db.dropDatabase();
+  });
+
+  it('/POST subjects', () => {
+    return request(app.getHttpServer())
+      .post('/subjects')
+      .send({ name: 'Invoice' })
+      .then((response) => {
+        expect(response.statusCode).toBe(HttpStatus.CREATED);
+        expect(response.body.created).toBeTruthy();
+      });
+  });
+
+  it('/GET subjects', () => {
+    return request(app.getHttpServer())
+      .get('/subjects')
+      .then((response) => {
+        expect(response.statusCode).toBe(HttpStatus.OK);
+        expect(response.body instanceof Array<Subject>).toBeTruthy();
+        expect(response.body[0].name).toBe('Invoice');
+      });
+  });
 });
--- backend/test/unit/interceptors/response-interceptor.test.spec.ts
+++ backend/test/unit/interceptors/response-interceptor.test.spec.ts
@@ -1,64 +1,66 @@
-import { lastValueFrom, of, tap } from "rxjs";
-import { HttpStatus } from "@nestjs/common";
-import { ExecutionContext } from "@nestjs/common/interfaces/features/execution-context.interface";
-import { CallHandler } from "@nestjs/common/interfaces/features/nest-interceptor.interface";
+import { lastValueFrom, of, tap } from 'rxjs';
+import { HttpStatus } from '@nestjs/common';
+import { ExecutionContext } from '@nestjs/common/interfaces/features/execution-context.interface';
+import { CallHandler } from '@nestjs/common/interfaces/features/nest-interceptor.interface';
 import { createMock } from '@golevelup/ts-jest';
-import { ResponseInterceptor } from "../../../src/interceptors/response.interceptor"
-import { IActionResult } from "../../../src/controllers/response-helpers/action-result.interface";
-import { NotFoundObjectResult } from "../../../src/controllers/response-helpers/notfound-result";
-import { InternalServerErrorObjectResult } from "../../../src/controllers/response-helpers/internal-server-error-result";
-import { CreatedObjectResult } from "../../../src/controllers/response-helpers/created-result";
-import { Reflector } from "@nestjs/core/services/reflector.service";
+import { ResponseInterceptor } from '../../../src/interceptors/response.interceptor';
+import { IActionResult } from '../../../src/controllers/response-helpers/action-result.interface';
+import { NotFoundObjectResult } from '../../../src/controllers/response-helpers/notfound-result';
+import { InternalServerErrorObjectResult } from '../../../src/controllers/response-helpers/internal-server-error-result';
+import { CreatedObjectResult } from '../../../src/controllers/response-helpers/created-result';
+import { Reflector } from '@nestjs/core/services/reflector.service';
 
 describe('Response Interceptor', () => {
-    let interceptor: ResponseInterceptor
-    let reflector: Reflector;
-  
-    beforeEach(() => {
-        reflector = new Reflector();
-        reflector.get = jest.fn().mockReturnValue(false);
-        interceptor = new ResponseInterceptor(reflector)
-    })
-  
+  let interceptor: ResponseInterceptor;
+  let reflector: Reflector;
 
-    test.each([
-        { actionResult: new NotFoundObjectResult(), expectedStatusCode: HttpStatus.NOT_FOUND},
-        { actionResult: new InternalServerErrorObjectResult({message:'Internal Server Error'}), expectedStatusCode: HttpStatus.INTERNAL_SERVER_ERROR },
-        { actionResult: new CreatedObjectResult({id:5050}), expectedStatusCode: HttpStatus.CREATED}
-    ])
-    ('intercept $actionResult should return status: $expectedStatusCode and body: $actionResult.body', async ({ actionResult, expectedStatusCode }) => {
+  beforeEach(() => {
+    reflector = new Reflector();
+    reflector.get = jest.fn().mockReturnValue(false);
+    interceptor = new ResponseInterceptor(reflector);
+  });
 
-        //Arrange
-        const mockedResponse = new MockedResponse();
-        const context = createMock<ExecutionContext>({
-            switchToHttp: () => ({
-                getResponse() {
-                   return mockedResponse;
-                },
-            }),
-        });
+  test.each([
+    { actionResult: new NotFoundObjectResult(), expectedStatusCode: HttpStatus.NOT_FOUND },
+    {
+      actionResult: new InternalServerErrorObjectResult({ message: 'Internal Server Error' }),
+      expectedStatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+    },
+    { actionResult: new CreatedObjectResult({ id: 5050 }), expectedStatusCode: HttpStatus.CREATED },
+  ])(
+    'intercept $actionResult should return status: $expectedStatusCode and body: $actionResult.body',
+    async ({ actionResult, expectedStatusCode }) => {
+      //Arrange
+      const mockedResponse = new MockedResponse();
+      const context = createMock<ExecutionContext>({
+        switchToHttp: () => ({
+          getResponse() {
+            return mockedResponse;
+          },
+        }),
+      });
 
-        const handler = createMock<CallHandler<IActionResult>>({
-            handle: () => of(actionResult)
-        });   
-        const ctx = context.switchToHttp();
-        const response: any = ctx.getResponse();
+      const handler = createMock<CallHandler<IActionResult>>({
+        handle: () => of(actionResult),
+      });
+      const ctx = context.switchToHttp();
+      const response: any = ctx.getResponse();
 
-        //Act
-        const observableResult = interceptor.intercept(context, handler);
-        const result = await lastValueFrom(observableResult);
+      //Act
+      const observableResult = interceptor.intercept(context, handler);
+      const result = await lastValueFrom(observableResult);
 
-        //Assert
-        expect(result).toBe(actionResult.body);
-        expect(mockedResponse.statusCode).toBe(expectedStatusCode);
-    })
-})
-
+      //Assert
+      expect(result).toBe(actionResult.body);
+      expect(mockedResponse.statusCode).toBe(expectedStatusCode);
+    },
+  );
+});
 
 class MockedResponse {
-    statusCode: number;
+  statusCode: number;
 
-    code(statusCode: number) {
-        this.statusCode = statusCode
-    };
+  code(statusCode: number) {
+    this.statusCode = statusCode;
+  }
 }
