b7c2f9b4fd4e81f54591e7ae62ab3542abe6605b
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-10 18:11:45 +0300

    introduce topic processor

--- backend/src/services/topic/topic.service.ts
+++ backend/src/services/topic/topic.service.ts
@@ -3,17 +3,23 @@ import { CreatedModel } from '../../repositories/helper-types';
 import { TopicsRepository } from '../../repositories/topic/repository';
 import { Topic } from '../../repositories/topic/schema';
 import { Subject } from '../../repositories/subject/schema';
+import { TopicProcessor } from './topic.processor';
 
 @Injectable()
 export class TopicService {
 
-    constructor(private readonly topicsRepository: TopicsRepository) {}
+    constructor(
+        private readonly topicProcessor: TopicProcessor,
+        private readonly topicsRepository: TopicsRepository) {}
 
     async create(topic: Topic) : Promise<CreatedModel> {
+        this.topicProcessor.validateEvent(topic.event)
         return await this.topicsRepository.create(topic);
     }
 
-    async getByEvent(event: string, subject: Subject) : Promise<Topic>{
-        return await this.topicsRepository.findOrCreate({ event: event, subject: subject});
+    async getOrCreateByEvent(event: string, subject: Subject) : Promise<Topic>{
+        this.topicProcessor.validateEvent(event)
+        const name = this.topicProcessor.getTopicNameFormEvent(event);
+        return await this.topicsRepository.findOrCreate({ event: event.toLowerCase(), name: name, subject: subject });
     }
 }
