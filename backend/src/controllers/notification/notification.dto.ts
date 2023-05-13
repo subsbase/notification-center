63adad9397b80593425303ecd47a0addfb628712
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-13 22:07:00 +0300

    add dtos validation

--- backend/src/controllers/notification/notification.dto.ts
+++ backend/src/controllers/notification/notification.dto.ts
@@ -1,7 +1,17 @@
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from 'class-validator';
 import { Payload } from '../../types/global-types';
 
 export class NotificationDto {
+
+  @IsNotEmpty({ message: 'subscribers ids (to) must be specifed' })
+  @IsArray({ message: 'subscribers ids (to) must be an array' })
+  @ArrayMinSize(1, { message: 'subscribers ids (to) must contain at least one element' })
   to: Array<string>;
+
+  @IsNotEmpty()
   payload: Payload;
+
+  @IsNotEmpty()
+  @IsString()
   actionUrl: string;
 }
