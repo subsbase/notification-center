f02ba4b98447bce012b38380c3f4c8c3e22b6140
Fahd Arafat <fahdarft@gmail.com>
2023-05-25 10:05:29 +0300

    send height to parent window

--- frontend/src/views/NotificationsBell.vue
+++ frontend/src/views/NotificationsBell.vue
@@ -1,12 +1,14 @@
 <template>
-  <p class="clickable notification-bell" @click="fetchAllNotifications()">
-    <span class="notification-count">{{ notificationCount }}</span>
-    <img class="clickable top-1 pos-relative" src="../assets/notification.png" />
-  </p>
+  <div class="clickable notification-bell" @click="fetchAllNotifications()">
+    <div class="py-5 px-5 pos-relative">
+      <span class="notification-count">{{ notificationCount }}</span>
+      <img class="clickable top-1" src="../assets/notification.png" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from 'vue'
+import { ref, onBeforeMount, onMounted } from 'vue'
 import { getAllNotifications } from '@/services/notifications'
 import { io } from 'socket.io-client'
 
@@ -15,10 +17,11 @@ import { getSubscriberId, getRealmHeader } from '../utils.js'
 const notificationCount = ref(0)
 const subscriberID = ref('')
 
-const socket = io(process.env.SERVER_BASE_URL, {
+const socket = io(process.env.VUE_APP_SERVER_BASE_URL, {
   extraHeaders: {
     'x-realm': getRealmHeader()
-  }
+  },
+  path: '/notifc/socket'
 })
 socket.on('connect', function () {
   socket.emit('joinGroup', subscriberID.value)
@@ -42,6 +45,10 @@ socket.on('NotificationArchived', function () {
 onBeforeMount(() => {
   subscriberID.value = getSubscriberId()
   fetchAllNotifications()
+})
+onMounted(() => {
+  const height = document.body.offsetHeight
+  window.parent.postMessage({ height }, '*')
 })
 
 const fetchAllNotifications = () => {
