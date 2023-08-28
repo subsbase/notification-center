bed70501653a6956e3cf5a7c7b232cc32481c878
NairuzY <84462600+NairuzY@users.noreply.github.com>
2023-08-28 16:17:25 +0300

    fix typo Dbconnection

--- backend/src/interceptors/interceptors.module.ts
+++ backend/src/interceptors/interceptors.module.ts
@@ -11,7 +11,7 @@ export class InterceptorsModule {
   static withConfig(dbConnection: string): DynamicModule {
     return {
       module: InterceptorsModule,
-      imports: [ServicesModule.withDbonnection(dbConnection)],
+      imports: [ServicesModule.withDbconnection(dbConnection)],
       providers: [
         {
           provide: APP_INTERCEPTOR,
